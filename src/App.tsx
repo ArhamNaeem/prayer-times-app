5b3e51d45815aebbae40f21d1a58697f1e63d7c4
Arham <arhamn205@gmail.com>
2023-01-18 20:55:57 +0500

    created main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Navbar from "./Navbar"
 import PrayerTime from "./PrayerTime"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 function App() {
+  const client = new QueryClient();
   return (
     <>
-        <Navbar />
-      {/* <PrayerTime/> */}
+      <div className="h-screen">
+        <QueryClientProvider client = {client}>
+          <Navbar />
+          <PrayerTime />
+        </QueryClientProvider>
+      </div>
     </>
- 
-  )
+  );
 }
 
 export default App
